fa389b5b04ff8dea894705d72a09791c160cf628
Md. Shakil Hossain <shakilhossainpavel@gmail.com>
2022-03-31 02:00:39 +0600

    basic structure

---
+++ src/main/java/com/paymentgateway/customerdashboardservice/CustomerDashboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paymentgateway.customerdashboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDashboardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerDashboardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paymentgateway/customerdashboardservice/CustomerDashboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentgateway.customerdashboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDashboardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
